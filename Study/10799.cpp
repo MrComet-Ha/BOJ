900791304c3f7600da64201ef1983eb6d4c92556
Hyeseong-Ha <mr.comet.ha@gmail.com>
2025-10-24 17:17:12 +0900

    Update 10799.cpp

--- Study/10799.cpp
+++ Study/10799.cpp
@@ -1,5 +1,4 @@
 #include <iostream>
-#include <stack>
 #include <string>
 
 using namespace std;
@@ -8,19 +7,19 @@ int main()
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
     string s;
-    int result = 0, count = 0;
+    int result = 0, count = 0;          // result : 결과값, count : 현재 누적된 파이프 수
     cin >> s;
     for (int i = 0; i < s.length(); ++i)
     {
-        if(s[i] == '(')
-            ++count;
-        else
+        if(s[i] == '(')                 // ( 라면 (쇠막대 시작 or 레이저 시작)
+            ++count;                    // 파이프 수 증가
+        else                            // ) 라면
         {
-            --count;
-            if(s[i-1] == '(')
-                result += count;
-            else
-                ++result;
+            --count;                    // 누적된 파이프 수 감소
+            if(s[i-1] == '(')           // 이전에 나온게 ( 라면 레이저이기 때문에
+                result += count;        // 현재 누적된 파이프 수를 그대로 추가
+            else                        // 이전에 나온게 ) 라면 파이프의 끝이기 때문에
+                ++result;               // 1만 증가
         }
     }
     cout << result << "\n";
